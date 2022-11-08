8608dc5074c3284def5fbcd7e51f759e35b05332
cloudtech-services <djapif@gmail.com>
2022-11-08 14:48:21 -0500

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -62,6 +62,15 @@ resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Allow all inbound HTTP traffic"
   vpc_id      = aws_vpc.vpc.id
+  
+  
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
